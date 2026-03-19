cc497541c308a1866f5df020ff13f690fd4e0202
uvivi161 <uvi69161@gmail.com>
2026-03-19 22:08:49 +0200

    change react-tilt to react-parallax-tilt

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -30,7 +30,6 @@ const Contact = () => {
   const handleSubmit = (e) => {
     e.preventDefault();
     setLoading(true);
-
     emailjs
       .send(
         import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
